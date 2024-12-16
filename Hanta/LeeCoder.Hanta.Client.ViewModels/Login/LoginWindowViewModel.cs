de9151ed309ed1261b77974363f98668ac8da1e2
Lee Coder <98294800+KR-JasonLane@users.noreply.github.com>
2024-12-16 19:39:56 +0900

    FIX : 로그인 뷰모델 바인딩 프로퍼티/커맨드 추가
    * CommunityToolkit 업데이트

--- Hanta/LeeCoder.Hanta.Client.ViewModels/Login/LoginWindowViewModel.cs
+++ Hanta/LeeCoder.Hanta.Client.ViewModels/Login/LoginWindowViewModel.cs
@@ -20,6 +20,24 @@ public partial class LoginWindowViewModel : HantaViewModelBase
 
     #region :: Properties ::
 
+    /// <summary>
+    /// 유저 아이디
+    /// </summary>
+    [ObservableProperty]
+    private string _userID = string.Empty;
+
+    /// <summary>
+    /// 유저 비밀번호
+    /// </summary>
+    [ObservableProperty]
+    private string _userPW = string.Empty;
+
+    /// <summary>
+    /// 아이디 저장 여부
+    /// </summary>
+    [ObservableProperty]
+    private bool _isSaveUserID = false;
+
     #endregion
 
 
@@ -28,6 +46,51 @@ public partial class LoginWindowViewModel : HantaViewModelBase
     #endregion
 
     #region :: Commands ::
+
+    /// <summary>
+    /// 서버설정 열기
+    /// </summary>
+    [RelayCommand]
+    private void OpenServerConfig()
+    {
+
+    }
+
+    /// <summary>
+    /// 창닫기
+    /// </summary>
+    [RelayCommand]
+    private void Close()
+    {
+
+    }
+
+    /// <summary>
+    /// 서버로 비밀번호 초기화 요청
+    /// </summary>
+    [RelayCommand]
+    private void RequestInitPassword()
+    {
+
+    }
+
+    /// <summary>
+    /// 서버로 로그인 요청
+    /// </summary>
+    [RelayCommand]
+    private void Login()
+    {
+
+    }
+
+    /// <summary>
+    /// 서버로 가입요청
+    /// </summary>
+    [RelayCommand]
+    private void RequestRegister()
+    {
+
+    }
 
     #endregion
 }
